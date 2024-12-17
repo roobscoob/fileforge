c8f75647f9b7b03677118e5a0cfb6dbb5011cab8
Rose Hall <rose@hall.ly>
2024-12-16 23:12:15 -0500

    allow DisplayableError to support no-alloc env

--- fileforge-lib/src/error/mod.rs
+++ fileforge-lib/src/error/mod.rs
@@ -1,5 +1,7 @@
 use core::fmt::Debug;
 
+use alloc::vec::Vec;
+
 use self::{
   render::{
     buffer::{
@@ -17,7 +19,6 @@ pub mod report;
 pub trait Error<const NODE_NAME_SIZE: usize> {
   fn with_report<Cb: FnMut(Report<NODE_NAME_SIZE>) -> ()>(&self, callback: Cb);
 
-  #[cfg(feature = "alloc")]
   fn into_display(self) -> DisplayableError<NODE_NAME_SIZE, Self>
   where
     Self: Sized,
@@ -26,13 +27,19 @@ pub trait Error<const NODE_NAME_SIZE: usize> {
   }
 }
 
-#[cfg(feature = "alloc")]
 pub struct DisplayableError<
   const DIAGNOSTIC_NODE_NAME_SIZE: usize,
   E: Error<DIAGNOSTIC_NODE_NAME_SIZE>,
 >(pub E);
 
-#[cfg(feature = "alloc")]
+struct S<'a> {
+  #[cfg(feature = "alloc")]
+  c: Vec<RenderBufferCell<'a>>,
+
+  #[cfg(not(feature = "alloc"))]
+  c: [RenderBufferCell<'a>; 80],
+}
+
 impl<const DIAGNOSTIC_NODE_NAME_SIZE: usize, E: Error<DIAGNOSTIC_NODE_NAME_SIZE>> Debug
   for DisplayableError<DIAGNOSTIC_NODE_NAME_SIZE, E>
 {
@@ -42,18 +49,31 @@ impl<const DIAGNOSTIC_NODE_NAME_SIZE: usize, E: Error<DIAGNOSTIC_NODE_NAME_SIZE>
     f.write_str("\n")?;
 
     self.0.with_report(|report| {
-      let mut buffer = RenderBuffer::dry();
-      let mut canvas = buffer.canvas_at(RenderPosition::zero());
-      canvas.write(&report).unwrap();
+      #[cfg(feature = "alloc")]
+      let mut slice: S = {
+        let mut buffer = RenderBuffer::dry();
+        let mut canvas = buffer.canvas_at(RenderPosition::zero());
+        canvas.write(&report).unwrap();
 
-      let width = buffer.highest_written_column + 1;
+        let width = buffer.highest_written_column + 1;
+        S {
+          c: alloc::vec![RenderBufferCell::default(); width],
+        }
+      };
 
-      let mut vec = alloc::vec![RenderBufferCell::default(); width];
-      let mut slice = vec.as_mut_slice();
+      #[cfg(not(feature = "alloc"))]
+      let mut slice: S = {
+        S {
+          c: [RenderBufferCell::default(); 80],
+        }
+      };
+
+      let mut slice = slice.c.as_mut_slice();
       let mut i = 0;
 
       loop {
-        let mut buffer = RenderBuffer::new(&mut slice, width, i);
+        let len = slice.len();
+        let mut buffer = RenderBuffer::new(&mut slice, len, i);
         let mut canvas = buffer.canvas_at(RenderPosition::zero());
 
         canvas.write(&report).unwrap();
