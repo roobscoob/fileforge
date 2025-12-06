995b15112987b657a5c8b8d76d70aff98f8c669e
Rose Hall <rose@hall.ly>
2025-12-06 17:42:08 -0500

    stop erroring!!

--- fileforge-nintendo/src/sead/sarc/sfat/mod.rs
+++ fileforge-nintendo/src/sead/sarc/sfat/mod.rs
@@ -31,22 +31,3 @@ pub struct SfatTable<'pool, UnderlyingStream: ReadableStream<Type = u8>> {
 // there's a weird logic hole.
 // if there's only one file in the SfatTable
 // then i cannot validate hash collisions
-
-impl<UnderlyingStream: ReadableStream<Type = u8>> SfatTable<UnderlyingStream> {
-  pub async fn into_entry(self, name: &[u8]) -> Result<Option<SfatEntry>, _> {
-    let name_hash = {
-      let hasher = SfntHasher::new(self.hash_multiplier, self.hash_mode);
-      hasher.write(name);
-      hasher.get_hash()
-    };
-
-    let matching_entries = self
-      .reader
-      .read_with(Contiguous::of::<SfatEntry>().with_item_count(self.header.file_count))
-      .await
-      .unwrap()
-      .filter(async |it| it.filename_hash == name_hash)
-      .collect::<heapless::Vec<SfatEntry, 255>>()
-      .await?;
-  }
-}
